4f1983fb06aff363cc42e9173888149ce0fa01cc
Pritam Das <pd26@iitbbs.ac.in>
2024-03-21 17:41:40 +0530

    package update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <GreetMessage />
+    <h1>Hello, {{ hello }}</h1>
+    <h1 v-html="channel"></h1>
+    <h1 v-text="channel"></h1>
+    <div v-html="hack"></div>
+    <button v-bind:disabled="disable" onclick="alert('may name is lakhan')">Bine me</button>
+    <h3 v-bind:class="status" class="underline">text underline</h3>
+    <h2 v-bind:class="isstatus ? 'new': 'old'">Movie? old</h2>
+    <div v-bind:style="{background:color,'font-size':num+'px'}"> yeh duniya yeh mehfil</div>
+     <!-- list rendering -->
+    <li v-bind:style="{textDecoration:none,
+    listStyle:none}" v-for="name in names" :key="name">{{ name }}</li>
+    <!-- event handeling -->
+    <h2 >{{ name }}</h2>
+    <button v-on:click="name='batman'">click me!</button>
+
+    
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import GreetMessage from './components/Greet.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      hello: 'namaste', // Removed the semicolon here
+      channel: `<b>salam namaste!!</b>'`,
+      hack:`<a href="#" onclick="alert('you have clicked me')">Click me!!</a>`,
+      //binding to attributes
+      disable:false,
+      status:'danger',
+      isstatus:false,
+      color:'red',
+      num:50,
+      names:["bruce","bayene","batman"],
+      name:'pritam'
+    }
+  },
+  components:{
+    GreetMessage,
   }
 }
 </script>
@@ -22,5 +55,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.underline
+{
+  text-decoration:underline ;
+}
+.new
+{
+  color:red;
+}
+
+.old
+{
+  color:green;
 }
 </style>
---
+++ src/components/Greet.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>Hello Pritam Bro</h2>
+</template>
+
+<script>
+export default
+    {
+        name: 'GreetMessage'
+    }
+</script>
+
+<style scoped></style>
